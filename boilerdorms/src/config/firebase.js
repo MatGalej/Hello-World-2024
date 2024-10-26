8698dd334bd7e04b5a2a6ed0c222b260cec244df
MatGalej <mgalej@purdue.edu>
2024-10-26 12:05:10 -0400

    Added firebase config

---
+++ boilerdorms/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUlUUVyX7G1Fnpq10Dw56bPyNLT8xSVCA",
+  authDomain: "boiler-dorms.firebaseapp.com",
+  projectId: "boiler-dorms",
+  storageBucket: "boiler-dorms.appspot.com",
+  messagingSenderId: "866464735986",
+  appId: "1:866464735986:web:ba0f20bfd9e344189e666b",
+  measurementId: "G-94D37MBPJ1"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
