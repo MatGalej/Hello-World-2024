aebe30cd1b7151af8bc315908b55e79230c002bc
MatGalej <mgalej@purdue.edu>
2024-10-26 13:31:23 -0400

    removed boilerplate code

--- boilerdorms/src/components/auth.jsx
+++ boilerdorms/src/components/auth.jsx
@@ -1,9 +1,11 @@
+import { auth } from '../config/firebase'
+
 export const Auth = () => {
-    return {
+    return (
         <div>
-        <input placeholder="Email..."/>
-        </input placeholder="Password...">
-        <button> Sign in </button>
+            <input placeholder="Email..."/>
+            <input placeholder="Password..." />
+            <button> Sign in </button>
         </div>
-    }
+    )
 }
