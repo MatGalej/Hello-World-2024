ad1d319d1f175586aa2aac3e6cfdb0f7570eafe8
MatGalej <mgalej@purdue.edu>
2024-10-26 12:35:36 -0400

    added firebase auth

--- boilerdorms/src/App.jsx
+++ boilerdorms/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Auth } from "./components/auth"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,9 +27,16 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className="App">
+        <auth />
+
+      </div>
+
     </>
   )
 }
---
+++ boilerdorms/src/components/auth.jsx
@@ -0,0 +1,9 @@
+export const Auth = () => {
+    return {
+        <div>
+        <input placeholder="Email..."/>
+        </input placeholder="Password...">
+        <button> Sign in </button>
+        </div>
+    }
+}
