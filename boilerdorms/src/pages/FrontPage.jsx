e208034252103478c535ba89377989d5a755b8d1
MatGalej <mgalej@purdue.edu>
2024-10-27 04:24:15 -0400

    added more button logic

--- boilerdorms/src/pages/FrontPage.jsx
+++ boilerdorms/src/pages/FrontPage.jsx
@@ -9,24 +9,15 @@ import ReviewBox from '../components/ReviewBox';
 import MapComponent from '../components/MapComponent';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-
-
-const logout = () => {
-  if (user) {
-    auth.signOut();
-  } else {
-    console.log("No user");
-  }
-}
-
-
 function FrontPage() {
   const [userVal, setUserVal] = useState(null);
+  const [success, SetSuccessLogin] = useState("");
 
   const Auth = getAuth();
   onAuthStateChanged(auth, (user) => {
     if (user) {
       setUserVal(user);
+      SetSuccessLogin(true);
     } else {
       console.log("No user");
     }
@@ -35,40 +26,64 @@ function FrontPage() {
   const logout = () => {
     if (userVal != null) {
       auth.signOut();
+      SetSuccessLogin(false);
       window.location.reload()
-    } 
+    }
   }
 
+  function hideElementAfterDelay(elementId, delay) {
+    setTimeout(() => {
+      const element = document.getElementById(elementId);
+      if (element) {
+        element.style.display = "none";
+      }
+    }, delay);
+  }
 
   return (
     <>
       <div className="background-image"></div>
 
       <header className='front-header'>
-        <a href='/Signup'>
-          <button id='Signup'>Sign Up</button>
-        </a>
-        
-        {userVal && (
-          <button className='logout-button' onClick={logout}>Log Out</button>
-        )}
+        <div className="user-header">
+          <div className='confirmation'>
+            {success && (
+              <div className='success-box'>
+                <p>Logged In Successfully!</p>
+              </div>
+            )}
+          </div>
 
-        {!userVal && (
-          <a href='/Login'>
-            <button id='Signin'>Log In</button>
-          </a>
-        )}
+          <div className='user-buttons'>
+            
+            {!userVal && (
+              <a href='/Signup'>
+                <button id='Signup'>Sign Up</button>
+              </a>
+            )}
+
+            {userVal && (
+              <button className='logout-button' onClick={logout}>Log Out</button>
+            )}
+              
+            {!userVal && (
+              <a href='/Login'>
+                <button id='Signin'>Log In</button>
+              </a>
+            )}
+          </div>
+        </div>
       </header>
 
+
+
       <div className='front-page'>
-        <div>
-          <h1>Boiler Dorms</h1>
-          <p>Discover what Purdue has to say about <br></br><strike>that one dorm</strike>, and all other, normal dorms.</p>
-          <div className='dorm-button-container'>
-            <a href='/Dorms'>
-              <button className='dorm-button'>View Dorms</button>
-            </a>
-          </div>
+        <h1>Boiler Dorms</h1>
+        <p>Discover what Purdue has to say about <br></br><strike>that one dorm</strike>, and all other, normal dorms.</p>
+        <div className='dorm-button-container'>
+          <a href='/Dorms'>
+            <button className='dorm-button'>View Dorms</button>
+          </a>
         </div>
       </div>
     </>
