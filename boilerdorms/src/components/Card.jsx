bb0b8f8222aaba35129876e3d30aca4a757dec76
Matthew Galej <mgalej@purdue.edu>
2024-11-20 20:33:56 -0500

    Added vercel json for nav

--- boilerdorms/src/components/Card.jsx
+++ boilerdorms/src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './../index.css';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 
 function Card({ title, text, link }) {
   return (
-    <Link to={link}> 
+    <Link to={link}>
       <div className="card">
         <h2 className="card-title">{title}</h2>
         <p className="card-text">{text}</p>
-    </div>
+      </div>
     </Link>
 
   );
